3cef6ee0c110c418de82be8a859b5ce4476f5bb8
Xavi002 <diogomartins200214@gmail.com>
2020-09-06 12:41:39 +0100

    ADDED: input and message state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+
+  console.log(input);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+
+      <input value={input} onChange={ event => setInput(event.target.value)} />
+      <button>Send Message</button>
     </div>
   );
 }
